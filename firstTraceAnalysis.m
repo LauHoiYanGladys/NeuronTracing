cda00df0d44c0a86311a6ea3b219e4fe764c4a12
DESKTOP-2RHR47P\Gladys Lau <lauhoiyangladys@gmail.com>
2017-07-11 01:15:31 +0800

    Added topological gene and dendrogram

--- firstTraceAnalysis.m
+++ firstTraceAnalysis.m
@@ -1,2 +1,23 @@
 % import a particular neuron tracing swc file, requires user input
+start_trees
+load_tree('sample2.mtr');
+%%
 fileName='filled neuron 2.swc';
+load_tree(fileName)
+trees{2}.frustum=1;
+plot_tree(trees{2})
+%%
+newtree=resample_tree(trees{2},10);
+sort_tree(newtree,'-LEX')
+%%
+figure('color','w')
+gene=gene_tree({{newtree}});
+%%
+newtree=repair_tree(newtree);
+%%
+figure('color','w')
+PL=PL_tree(newtree);
+dendrogram_tree(newtree,PL/35,PL,PL,[], [], '-p -v')
+colorbar
+title('Dendrogram of neuron 2')
+ylabel('topological path length')
